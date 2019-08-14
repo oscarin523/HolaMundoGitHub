758629165e213e2ed89f25238e59f4753f044228
Alumno <Alumno@PC24>
2019-08-14 12:28:29 -0500

    Codigo github c#

--- Hola mundo/Hola mundo/Program.cs
+++ Hola mundo/Hola mundo/Program.cs
@@ -6,7 +6,15 @@ namespace Hola_mundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int valor1, valor2, respuesta;
+
+            Console.WriteLine("Vamos a sumar dos numeros:");
+            Console.WriteLine("inserta el valor 1 ");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserta el valor 2 ");
+            valor2 = int.Parse(Console.ReadLine());
+            respuesta = valor1 + valor2;
+            
         }
     }
 }
